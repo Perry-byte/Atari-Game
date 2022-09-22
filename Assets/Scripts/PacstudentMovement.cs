1007872e7a019fc7e59ee88985199a93ecfe13b2
Perry <79495144+Perry-byte@users.noreply.github.com>
2022-09-22 22:39:55 +1000

    Pacstudent Rotations Added

--- Assets/Scripts/PacstudentMovement.cs
+++ Assets/Scripts/PacstudentMovement.cs
@@ -6,25 +6,16 @@ public class PacstudentMovement : MonoBehaviour
 {
     [SerializeField]
     private GameObject item;
-    // private List<GameObject> itemList;
     private Tweener tweener;
     private bool one = false;
     private bool two = false;
     private bool three = false;
     private bool four = false;
-    private Vector3 right = new Vector3(-11.95f, 4.71f, 2.21f);
-    private Vector3 left = new Vector3(-19.0f, -1.2f, 2.21f);
-    private Vector3 up = new Vector3(-19.0f, 4.71f, 2.21f);
-    private Vector3 down = new Vector3(-11.95f, -1.2f, 2.21f);
 
     // Start is called before the first frame update
     void Start()
     {
         tweener = GetComponent<Tweener>();
-        // item.GetComponent<Animation>().Play();
-        // StartCoroutine(Move1());
-        // itemList = new List<GameObject>();
-        // itemList.Add(item);
     }
 
     // Update is called once per frame
@@ -32,24 +23,28 @@ public class PacstudentMovement : MonoBehaviour
     {
         if (gameObject.GetComponent<Tweener>().tweenStatus() && one == false)
         {
+            item.transform.rotation = Quaternion.identity;
             tweener.AddTween(item.transform, item.transform.position, new Vector3(-11.95f, 4.71f, 2.21f), 1.5f);
             one = true;
         }
 
         if (gameObject.GetComponent<Tweener>().tweenStatus() && two == false)
         {
+            item.transform.Rotate(0.0f,0.0f,-90.0f, Space.World);
             tweener.AddTween(item.transform, item.transform.position, new Vector3(-11.95f, -1.2f, 2.21f), 1.5f);
             two = true;
         }
 
         if (gameObject.GetComponent<Tweener>().tweenStatus() && three == false)
         {
+            item.transform.Rotate(0.0f, 180.0f, 90.0f, Space.World);
             tweener.AddTween(item.transform, item.transform.position, new Vector3(-19.0f, -1.2f, 2.21f), 1.5f);
             three = true;
         }
 
         if (gameObject.GetComponent<Tweener>().tweenStatus() && four == false)
         {
+            item.transform.Rotate(0.0f, 180.0f, 270.0f, Space.World);
             tweener.AddTween(item.transform, item.transform.position, new Vector3(-19.0f, 4.71f, 2.21f), 1.5f);
             one = false;
             two = false;
